4b38dd0c90616f8ea1c8be7d1f833aebdf499eb8
robottao <15425084+robottao@user.noreply.gitee.com>
2025-07-11 09:03:04 +0800

    Add mobilenetv3

--- hdd/models/nn_utils.py
+++ hdd/models/nn_utils.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch.nn as nn
 
 
@@ -27,3 +29,19 @@ def activation_from_name(name: str) -> nn.Module:
 def count_trainable_parameter(model: nn.Module) -> int:
     """Get the total number trainable parameters in the model."""
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def make_divisible(v: float, divisor: int, min_value: Optional[int] = None) -> int:
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
