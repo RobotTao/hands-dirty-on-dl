bddff71d08e966a0f7044d6cd39dbf64be469066
robottao <15425084+robottao@user.noreply.gitee.com>
2025-08-07 22:31:34 +0800

    Add viz script

--- hdd/visualization/vis_utils.py
+++ hdd/visualization/vis_utils.py
@@ -3,7 +3,13 @@
 from builtins import range
 from math import ceil, sqrt
 
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from torchvision import tv_tensors
+from torchvision.transforms import v2
+from torchvision.transforms.v2 import functional as F
+from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
 
 
 def visualize_grid(Xs, ubound=255.0, padding=1):
@@ -36,3 +42,59 @@ def visualize_grid(Xs, ubound=255.0, padding=1):
         y0 += H + padding
         y1 += H + padding
     return grid
+
+
+def det_seg_plot(imgs, row_title=None, bbox_width=3, **imshow_kwargs):
+    """Plot image, detection and segmentation results in a grid."""
+    if not isinstance(imgs[0], list):
+        # Make a 2d grid even if there's just 1 row
+        imgs = [imgs]
+
+    num_rows = len(imgs)
+    num_cols = len(imgs[0])
+    _, axs = plt.subplots(nrows=num_rows, ncols=num_cols, squeeze=False)
+    for row_idx, row in enumerate(imgs):
+        for col_idx, img in enumerate(row):
+            boxes = None
+            masks = None
+            if isinstance(img, tuple):
+                img, target = img
+                if isinstance(target, dict):
+                    boxes = target.get("boxes")
+                    masks = target.get("masks")
+                elif isinstance(target, tv_tensors.BoundingBoxes):
+                    boxes = target
+
+                    # Conversion necessary because draw_bounding_boxes() only
+                    # work with this specific format.
+                    # if tv_tensors.is_rotated_bounding_format(boxes.format):
+                    #     boxes = v2.ConvertBoundingBoxFormat("xyxyxyxy")(boxes)
+                else:
+                    raise ValueError(f"Unexpected target type: {type(target)}")
+            img = F.to_image(img)
+            if img.dtype.is_floating_point and img.min() < 0:
+                # Poor man's re-normalization for the colors to be OK-ish. This
+                # is useful for images coming out of Normalize()
+                img -= img.min()
+                img /= img.max()
+
+            img = F.to_dtype(img, torch.uint8, scale=True)
+            if boxes is not None:
+                img = draw_bounding_boxes(img, boxes, colors="yellow", width=bbox_width)
+            if masks is not None:
+                img = draw_segmentation_masks(
+                    img,
+                    masks.to(torch.bool),
+                    colors=["green"] * masks.shape[0],
+                    alpha=0.65,
+                )
+
+            ax = axs[row_idx, col_idx]
+            ax.imshow(img.permute(1, 2, 0).numpy(), **imshow_kwargs)
+            ax.set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+
+    if row_title is not None:
+        for row_idx in range(num_rows):
+            axs[row_idx, 0].set(ylabel=row_title[row_idx])
+
+    plt.tight_layout()
